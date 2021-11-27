0e632d892d0a86c861f5b7156318ba0c039b9357
David Huber <dave@yomi.eu>
2021-11-27 19:21:39 +0100

    Revert "Extend ethier.sign.addresses to reading mnemonic from a file"
    This reverts commit 651d78893430625de3882288f7492b2aa0a6f7b3.

--- ethier/sign.go
+++ ethier/sign.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/divergencetech/ethier/eth"
@@ -22,7 +21,6 @@ func init() {
 	}
 
 	rootCmd.AddCommand(signCmd)
-	signCmd.PersistentFlags().String("mnemonic", "", "Path to file containing the mnemonic, password and account number (newline separated) for key derivation")
 
 	var signAddrCmd = &cobra.Command{
 		Use:   "addresses",
@@ -38,68 +36,17 @@ type SignedAddress struct {
 	Signature string `json:"signature"`
 }
 
-func getSignerFromMnemonic(filepath string) (*eth.Signer, error) {
-	dat, err := os.ReadFile(filepath)
-	if err != nil {
-		return nil, fmt.Errorf("Read mnemonic: %v", err)
-	}
-
-	lines := strings.Split(string(dat), "\n")
-
-	mnemonic := lines[0]
-	var password string = ""
-	var account uint = 0
-
-	if len(lines) > 1 {
-		password = lines[1]
-	}
-
-	if len(lines) > 2 {
-		if lines[2] != "" {
-			acc, err := strconv.Atoi(lines[2])
-			if err != nil {
-				return nil, fmt.Errorf("Converting derivation account: %v", err)
-			}
-			account = uint(acc)
-		}
-	}
-
-	signer, err := eth.DefaultHDPathPrefix.SignerFromSeedPhrase(
-		mnemonic, password, account,
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("Generate signer: %v", err)
-	}
-	return signer, nil
-}
-
-// sign addresses generates a new signer or derives it from a given mnemonic
-// to sign a list of addresses
-func signAddresses(cmd *cobra.Command, args []string) (retErr error) {
+// sign generates a new signer and signs a given message
+func signAddresses(_ *cobra.Command, args []string) (retErr error) {
 	defer func() {
 		if retErr != nil {
 			retErr = fmt.Errorf("signing: %w", retErr)
 		}
 	}()
 
-	mnemonicPath, err := cmd.Flags().GetString("mnemonic")
+	signer, err := eth.NewSigner(256)
 	if err != nil {
-		log.Fatalf("Get mnemonics flag: %v", err)
-	}
-
-	var signer *eth.Signer
-
-	if mnemonicPath != "" {
-		signer, err = getSignerFromMnemonic(mnemonicPath)
-		if err != nil {
-			log.Fatalf("Generate signer from mnemonic: %v", err)
-		}
-	} else {
-		signer, err = eth.NewSigner(256)
-		if err != nil {
-			log.Fatalf("Generate new signer: %v", err)
-		}
+		log.Fatalf("Generate signer: %v", err)
 	}
 
 	buf, err := io.ReadAll(os.Stdin)
