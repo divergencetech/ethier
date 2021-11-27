3d48b77c0f3ade1eba8ca84a69ff3a1fd0bb1ea8
David Huber <dave@yomi.eu>
2021-11-27 18:52:10 +0100

    Remove MessageGenerator and move the relevant functions to SignedAllowance

--- contracts/crypto/SignedAllowance.sol
+++ contracts/crypto/SignedAllowance.sol
@@ -4,14 +4,13 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./SignatureChecker.sol";
-import "./MessageGenerator.sol";
 
 /**
 @title SignedAllowance
 @notice Additional functions for EnumerableSet.Addresset that require a valid
 ECDSA signature of a standardized message, signed by any member of the set.
 @dev This is a convenience library combining the functionalities of 
-{SignatureChecker} and {MessageGenerator}.
+{SignatureChecker} + message generation.
  */
 library SignedAllowance {
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -33,7 +32,7 @@ library SignedAllowance {
         bytes calldata signature,
         mapping(bytes32 => bool) storage usedMessages
     ) internal {
-        bytes32 message = MessageGenerator.generateMessage(data);
+        bytes32 message = generateMessage(data);
         return signers.validateSignature(message, signature, usedMessages);
     }
 
@@ -46,7 +45,7 @@ library SignedAllowance {
         bytes memory data,
         bytes calldata signature
     ) internal view {
-        bytes32 message = MessageGenerator.generateMessage(data);
+        bytes32 message = generateMessage(data);
         return signers.validateSignature(message, signature);
     }
 
@@ -59,7 +58,27 @@ library SignedAllowance {
         address addr,
         bytes calldata signature
     ) internal view {
-        bytes32 message = MessageGenerator.generateMessage(addr);
+        bytes32 message = generateMessage(addr);
         return signers.validateSignature(message, signature);
+    }
+
+    /**
+    @notice Generates a message for a given data input that will be signed off-chain using ECDSA.
+    @dev For multiple data fields, a standard concatenation using 
+    `abi.encodePacked` is commonly used to build data.
+     */
+    function generateMessage(bytes memory data)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(data);
+    }
+
+    /**
+    @notice Generates a message for a given address.
+     */
+    function generateMessage(address address_) internal pure returns (bytes32) {
+        return generateMessage(abi.encodePacked(address_));
     }
 }
