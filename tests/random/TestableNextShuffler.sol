feb8741d9f775c5dbfe3910ad5a63a6f6f28751a
Divergence arran@divergencetech.xyz <me@arranschlosberg.com>
2021-11-29 13:25:56 +0000

    Implementation of random/NextShuffler; NOTE the important @dev comment re security.

---
+++ tests/random/TestableNextShuffler.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021 Divergent Technologies Ltd (github.com/divergencetech)
+pragma solidity >=0.8.0 <0.9.0;
+
+import "../../contracts/random/NextShuffler.sol";
+import "../../contracts/random/PRNG.sol";
+
+contract TestableNextShuffler is NextShuffler {
+    constructor(uint256 numToShuffle) NextShuffler(numToShuffle) {}
+
+    uint256[] public permutation;
+
+    function permute(uint64 seed) external {
+        permute(keccak256(abi.encodePacked(seed)));
+    }
+
+    function permute(bytes32 seed) public {
+        PRNG.Source src = PRNG.newSource(seed);
+        for (uint256 i = 0; i < NextShuffler.NUM_TO_SHUFFLE; i++) {
+            permutation.push(NextShuffler._next(src));
+        }
+    }
+}
