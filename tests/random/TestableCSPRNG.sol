798d12c61c938d8e62455012a6188303cb88a383
David Huber <dave@yomi.eu>
2021-12-10 10:58:33 +0100

    Reimport previous version of the PRNG as CSPRNG and adapt the testing suite to cover multiple implementations

--- tests/random/TestableCSPRNG.sol
+++ tests/random/TestableCSPRNG.sol
@@ -2,12 +2,12 @@
 // Copyright (c) 2021 the ethier authors (github.com/divergencetech/ethier)
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../../contracts/random/PRNG.sol";
+import "../../contracts/random/CSPRNG.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 /// @notice Testing contract that exposes functionality of the PRNG library.
-contract TestablePRNG {
-    using PRNG for PRNG.Source;
+contract TestableCSPRNG {
+    using CSPRNG for CSPRNG.Source;
     using Strings for uint256;
 
     /**
@@ -17,6 +17,8 @@ contract TestablePRNG {
     part of the public API.
      */
     struct State {
+        uint256 seed;
+        uint256 counter;
         uint256 entropy;
         uint256 remain;
     }
@@ -31,7 +33,7 @@ contract TestablePRNG {
         uint16 n
     ) private pure returns (uint256[] memory, State memory) {
         // NB! Read the documentation of PRNG re unpredictability.
-        PRNG.Source src = PRNG.newSource(seed);
+        CSPRNG.Source src = CSPRNG.newSource(seed);
 
         uint256[] memory samples = new uint256[](n);
         for (uint256 i = 0; i < n; i++) {
@@ -39,7 +41,7 @@ contract TestablePRNG {
         }
 
         State memory state;
-        (state.entropy, state.remain) = src.state();
+        (state.seed, state.counter, state.entropy, state.remain) = src.state();
 
         return (samples, state);
     }
@@ -60,9 +62,9 @@ contract TestablePRNG {
         (, state) = _sample(seed, bits, n);
     }
 
-    /// @dev Exposes PRNG.bitLength().
+    /// @dev Exposes CSPRNG.bitLength().
     function bitLength(uint256 n) public pure returns (uint256) {
-        return PRNG.bitLength(n);
+        return CSPRNG.bitLength(n);
     }
 
     /// @dev Returns n samples in [0,max).
@@ -72,11 +74,11 @@ contract TestablePRNG {
         uint16 n
     ) public pure returns (uint256[] memory) {
         // NB! Read the documentation of PRNG re unpredictability.
-        PRNG.Source src = PRNG.newSource(seed);
+        CSPRNG.Source src = CSPRNG.newSource(seed);
 
         // As all samples have the same upper bound, calculate the bit length
         // once and reuse it.
-        uint16 bits = PRNG.bitLength(max);
+        uint16 bits = CSPRNG.bitLength(max);
 
         uint256[] memory samples = new uint256[](n);
         for (uint256 i = 0; i < n; i++) {
@@ -99,24 +101,36 @@ contract TestablePRNG {
         uint16 bits,
         uint16 beforeStore
     ) public {
-        PRNG.Source src = PRNG.newSource(seed);
+        CSPRNG.Source src = CSPRNG.newSource(seed);
         for (uint256 i = 0; i < beforeStore; i++) {
             src.read(bits);
         }
 
         src.store(storedSource);
-        PRNG.Source copy = PRNG.loadSource(storedSource);
+        CSPRNG.Source copy = CSPRNG.loadSource(storedSource);
 
         // Confirm that we've actually round-tripped the internal state via
         // storage and not just copied it.
         require(
-            PRNG.Source.unwrap(src) != PRNG.Source.unwrap(copy),
+            CSPRNG.Source.unwrap(src) != CSPRNG.Source.unwrap(copy),
             "Identical Sources, not a copy"
         );
 
-        (uint256 entropy0, uint256 remain0) = src.state();
-        (uint256 entropy1, uint256 remain1) = copy.state();
+        (
+            uint256 seed0,
+            uint256 counter0,
+            uint256 entropy0,
+            uint256 remain0
+        ) = src.state();
+        (
+            uint256 seed1,
+            uint256 counter1,
+            uint256 entropy1,
+            uint256 remain1
+        ) = copy.state();
 
+        require(seed0 == seed1, "Seeds differ");
+        require(counter0 == counter1, "Counters differ");
         require(remain0 == remain1, "Remaining bits differ");
         // Test the entropy last as it's derived from the other values so a
         // revert() from one of them is more informative.
--- tests/random/prng_test.go
+++ tests/random/prng_test.go
@@ -9,11 +9,13 @@ import (
 
 	"github.com/divergencetech/ethier/ethtest"
 	"github.com/dustin/go-humanize"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/google/go-cmp/cmp"
 )
 
-func deploy(t *testing.T) (*ethtest.SimulatedBackend, *TestablePRNG) {
+func deployPRNG(t *testing.T) (*ethtest.SimulatedBackend, *TestablePRNG) {
 	t.Helper()
 
 	sim := ethtest.NewSimulatedBackendTB(t, 1)
@@ -25,8 +27,54 @@ func deploy(t *testing.T) (*ethtest.SimulatedBackend, *TestablePRNG) {
 	return sim, prng
 }
 
-// Abs returns the absolute value of x.
-func Abs(x int64) int64 {
+func deployCSPRNG(t *testing.T) (*ethtest.SimulatedBackend, *TestableCSPRNG) {
+	t.Helper()
+
+	sim := ethtest.NewSimulatedBackendTB(t, 1)
+	_, _, prng, err := DeployTestableCSPRNG(sim.Acc(0), sim)
+	if err != nil {
+		t.Fatalf("DeployTestablePRNG() error %v", err)
+	}
+
+	return sim, prng
+}
+
+type TestingContract interface {
+	Sample(opts *bind.CallOpts, seed [32]byte, bits uint16, n uint16) ([]*big.Int, error)
+	BitLength(opts *bind.CallOpts, n *big.Int) (*big.Int, error)
+	ReadLessThan(opts *bind.CallOpts, seed [32]byte, max *big.Int, n uint16) ([]*big.Int, error)
+	TestStoreAndLoad(opts *bind.TransactOpts, seed [32]byte, bits uint16, beforeStore uint16) (*types.Transaction, error)
+}
+
+type Implementation struct {
+	name string
+	sim  *ethtest.SimulatedBackend
+	prng TestingContract
+}
+
+func getImplementations(t *testing.T) []Implementation {
+	var impl []Implementation
+	{
+		sim, prng := deployPRNG(t)
+		impl = append(impl, Implementation{
+			name: "PRNG",
+			sim:  sim,
+			prng: prng,
+		})
+	}
+	{
+		sim, prng := deployCSPRNG(t)
+		impl = append(impl, Implementation{
+			name: "CSPRNG",
+			sim:  sim,
+			prng: prng,
+		})
+	}
+
+	return impl
+}
+
+func abs(x int64) int64 {
 	if x < 0 {
 		return -x
 	}
@@ -37,8 +85,7 @@ func normalCDF(z float64) float64 {
 	return 0.5 + 0.5*math.Erf(z/math.Sqrt2)
 }
 
-func TestRead(t *testing.T) {
-	_, prng := deploy(t)
+func TestReadProbabilistic(t *testing.T) {
 	tests := []struct {
 		seedFrom                  string
 		bitsPerSample, numSamples uint64
@@ -71,187 +118,312 @@ func TestRead(t *testing.T) {
 			var seed [32]byte
 			copy(seed[:], crypto.Keccak256([]byte(tt.seedFrom)))
 
-			samples, state, err := prng.Sample(nil, seed, uint16(tt.bitsPerSample), uint16(tt.numSamples))
+			for _, ii := range getImplementations(t) {
+				t.Run(ii.name, func(t *testing.T) {
+
+					samples, err := ii.prng.Sample(nil, seed, uint16(tt.bitsPerSample), uint16(tt.numSamples))
+					if err != nil {
+						t.Fatalf("Sample(%x, 8 bits, 1000 samples) error %v", seed, err)
+					}
+
+					var min, max, sum uint64
+					var onesCount int
+
+					numMax := (1 << tt.bitsPerSample) - 1
+					distMean := float64(numMax) / 2.
+					distVar := float64(numMax*numMax) / 12.
+
+					min = math.MaxUint64
+
+					var cusum, cusumMax int64
+
+					for i, s := range samples {
+						if !s.IsUint64() {
+							t.Fatalf("sample[%d].IsUint64() got false; want true", i)
+						}
+
+						u := s.Uint64()
+						sum += u
+						if u < min {
+							min = u
+						}
+						if u > max {
+							max = u
+						}
+
+						onesCount += bits.OnesCount64(u)
+
+						for j := uint64(0); j < tt.bitsPerSample; j++ {
+							z := 2*int64((u>>j)&1) - 1
+							cusum += z
+							abs := abs(cusum)
+							if abs > cusumMax {
+								cusumMax = abs
+							}
+						}
+					}
+
+					t.Run("MinMax", func(t *testing.T) {
+						// Extremely unlikely that neither the min nor the max are
+						// returned given the high number of samples and the small
+						// sampling spaces.
+						if min != 0 {
+							t.Errorf("Min = %d; want 0", min)
+						}
+						if want := uint64(1<<tt.bitsPerSample) - 1; max != want {
+							t.Errorf("Max = %d; want %d", max, want)
+						}
+					})
+
+					t.Run("Sum", func(t *testing.T) {
+
+						expectedSum := distMean * float64(tt.numSamples)
+						// The variance of as sum of uniform random variables in
+						// [0,sampleMax] is given by the sum of the individual variances
+						twoSigmaRelDev := 2 * math.Sqrt(distVar*float64(tt.numSamples)) / expectedSum
+
+						if dev := 1 - float64(expectedSum)/float64(sum); math.Abs(dev) > twoSigmaRelDev {
+							t.Errorf("Sum = %s deviates by %.2f%% (>%.2f%% abs = 2 sigma) from expected sum %s", humanize.Comma(int64(sum)), dev*100, twoSigmaRelDev*100, humanize.Comma(int64(expectedSum)))
+						}
+					})
+
+					t.Run("Monobit Frequency", func(t *testing.T) {
+						expectedMonocount := float64(tt.numSamples*tt.bitsPerSample) / 2
+						// Follows a binomial distribution. Variance is nSamples/4
+						twoSigmaRelDev := 2. / math.Sqrt(float64(tt.numSamples*tt.bitsPerSample))
+
+						if dev := 1 - float64(expectedMonocount)/float64(onesCount); math.Abs(dev) > twoSigmaRelDev {
+							t.Errorf("One count = %s deviates by %.2f%% (>%.2f%% abs = 2 sigma) from expected sum %s", humanize.Comma(int64(onesCount)), dev*100, twoSigmaRelDev*100, humanize.Comma(int64(expectedMonocount)))
+						}
+					})
+
+					t.Run("Monobit CUSUM", func(t *testing.T) {
+						// See Sect. 2.13 in https://nvlpubs.nist.gov/nistpubs/legacy/sp/nistspecialpublication800-22r1a.pdf
+						p := float64(1)
+						n := tt.numSamples * tt.bitsPerSample
+						z := cusumMax
+						sqrtn := math.Sqrt(float64(n))
+						kStart := int64(0.25 * (-float64(n)/float64(z) + 1))
+						kEnd := int64(0.25 * (float64(n)/float64(z) - 1))
+
+						for k := kStart; k <= kEnd; k++ {
+							p -= normalCDF(float64((4*k+1)*z)/sqrtn) - normalCDF(float64((4*k-1)*z)/sqrtn)
+						}
+
+						kStart = int64(0.25 * (-float64(n)/float64(z) - 3))
+						for k := kStart; k <= kEnd; k++ {
+							p += normalCDF(float64((4*k+3)*z)/sqrtn) - normalCDF(float64((4*k+1)*z)/sqrtn)
+						}
+
+						if p < 0.01 {
+							t.Errorf("Cusum test failed with p=%.2f < 0.01", p)
+						}
+					})
+
+				})
+
+			}
+		})
+	}
+}
+
+func TestReadInternalStatePRNG(t *testing.T) {
+	tests := []struct {
+		seedFrom                  string
+		bitsPerSample, numSamples uint64
+	}{
+		{
+			seedFrom:      "a",
+			bitsPerSample: 8,
+			numSamples:    5_000,
+		},
+		{
+			seedFrom:      "b",
+			bitsPerSample: 5,
+			numSamples:    5_000,
+		},
+		{
+			seedFrom:      "c",
+			bitsPerSample: 7,
+			numSamples:    10_000,
+		},
+		{
+			seedFrom:      "d",
+			bitsPerSample: 3,
+			numSamples:    7_500,
+		},
+	}
+
+	for _, tt := range tests {
+		name := fmt.Sprintf("seed keccak256(%q) %d samples of %d bits", tt.seedFrom, tt.numSamples, tt.bitsPerSample)
+		t.Run(name, func(t *testing.T) {
+			_, prng := deployPRNG(t)
+
+			var seed [32]byte
+			copy(seed[:], crypto.Keccak256([]byte(tt.seedFrom)))
+
+			state, err := prng.SampleState(nil, seed, uint16(tt.bitsPerSample), uint16(tt.numSamples))
 			if err != nil {
 				t.Fatalf("Sample(%x, 8 bits, 1000 samples) error %v", seed, err)
 			}
 
-			t.Run("probabilistic tests", func(t *testing.T) {
-				var min, max, sum uint64
-				var onesCount int
+			// // This test is primarily in place to confirm that the assembly
+			// // implementation works as intended when converted to a
+			// // high-level, more readable equivalent.
 
-				numMax := (1 << tt.bitsPerSample) - 1
-				distMean := float64(numMax) / 2.
-				distVar := float64(numMax*numMax) / 12.
+			seedInt := new(big.Int).SetBytes(seed[:])
+			two128 := new(big.Int).Lsh(big.NewInt(1), 128)
 
-				min = math.MaxUint64
+			// first 128 bits of seed
+			carry := new(big.Int).Rsh(seedInt, 128)
 
-				var cusum, cusumMax int64
+			// last 128 bits of seed
+			number := new(big.Int).Mod(seedInt, two128)
 
-				for i, s := range samples {
-					if !s.IsUint64() {
-						t.Fatalf("sample[%d].IsUint64() got false; want true", i)
-					}
+			factor := new(big.Int).Sub(two128, big.NewInt(10408))
 
-					u := s.Uint64()
-					sum += u
-					if u < min {
-						min = u
-					}
-					if u > max {
-						max = u
-					}
+			// Performing MWC updates
+			nRefills := (tt.numSamples * tt.bitsPerSample) / 128
+			for k := uint64(0); k < nRefills; k++ {
+				tmp := new(big.Int).Mul(factor, number)
+				tmp.Add(tmp, carry)
 
-					onesCount += bits.OnesCount64(u)
+				carry.Rsh(tmp, 128)
+				number.Mod(tmp, two128)
+			}
 
-					for j := uint64(0); j < tt.bitsPerSample; j++ {
-						z := 2*int64((u>>j)&1) - 1
-						cusum += z
-						abs := Abs(cusum)
-						if abs > cusumMax {
-							cusumMax = abs
-						}
-					}
-				}
+			carry.Lsh(carry, 128)
+			entropy := new(big.Int).Add(carry, number)
 
-				// Extremely unlikely that neither the min nor the max are
-				// returned given the high number of samples and the small
-				// sampling spaces.
-				if min != 0 {
-					t.Errorf("Min = %d; want 0", min)
-				}
-				if want := uint64(1<<tt.bitsPerSample) - 1; max != want {
-					t.Errorf("Max = %d; want %d", max, want)
-				}
+			wantState := TestablePRNGState{
+				Entropy: entropy,
+				Remain:  big.NewInt(128 - int64(tt.bitsPerSample*tt.numSamples)%128),
+			}
 
-				// Random number sum
-				{
-					expectedSum := distMean * float64(tt.numSamples)
-					// The variance of as sum of uniform random variables in
-					// [0,sampleMax] is given by the sum of the individual variances
-					twoSigmaRelDev := 2 * math.Sqrt(distVar*float64(tt.numSamples)) / expectedSum
+			if diff := cmp.Diff(wantState, state, ethtest.Comparers()...); diff != "" {
+				t.Errorf("After %d samples of %d bits each; internal state diff (-want +got):\n%s", tt.numSamples, tt.bitsPerSample, diff)
+			}
+		})
+	}
+}
 
-					if dev := 1 - float64(expectedSum)/float64(sum); math.Abs(dev) > twoSigmaRelDev {
-						t.Errorf("Sum = %s deviates by %.2f%% (>%.2f%% abs = 2 sigma) from expected sum %s", humanize.Comma(int64(sum)), dev*100, twoSigmaRelDev*100, humanize.Comma(int64(expectedSum)))
-					}
-				}
+func TestReadInternalStateCSPRNG(t *testing.T) {
+	tests := []struct {
+		seedFrom                  string
+		bitsPerSample, numSamples uint64
+	}{
+		{
+			seedFrom:      "a",
+			bitsPerSample: 8,
+			numSamples:    5_000,
+		},
+		{
+			seedFrom:      "b",
+			bitsPerSample: 5,
+			numSamples:    5_000,
+		},
+		{
+			seedFrom:      "c",
+			bitsPerSample: 7,
+			numSamples:    10_000,
+		},
+		{
+			seedFrom:      "d",
+			bitsPerSample: 3,
+			numSamples:    7_500,
+		},
+	}
 
-				// Frequency monobits
-				{
-					expectedMonocount := float64(tt.numSamples*tt.bitsPerSample) / 2
-					// Follows a binomial distribution. Variance is nSamples/4
-					twoSigmaRelDev := 2. / math.Sqrt(float64(tt.numSamples*tt.bitsPerSample))
+	for _, tt := range tests {
+		name := fmt.Sprintf("seed keccak256(%q) %d samples of %d bits", tt.seedFrom, tt.numSamples, tt.bitsPerSample)
+		t.Run(name, func(t *testing.T) {
+			_, prng := deployCSPRNG(t)
 
-					if dev := 1 - float64(expectedMonocount)/float64(onesCount); math.Abs(dev) > twoSigmaRelDev {
-						t.Errorf("One count = %s deviates by %.2f%% (>%.2f%% abs = 2 sigma) from expected sum %s", humanize.Comma(int64(onesCount)), dev*100, twoSigmaRelDev*100, humanize.Comma(int64(expectedMonocount)))
-					}
-				}
+			var seed [32]byte
+			copy(seed[:], crypto.Keccak256([]byte(tt.seedFrom)))
 
-				// Cusum
-				// See Sect. 2.13 in https://nvlpubs.nist.gov/nistpubs/legacy/sp/nistspecialpublication800-22r1a.pdf
-				{
-					p := float64(1)
-					n := tt.numSamples * tt.bitsPerSample
-					z := cusumMax
-					sqrtn := math.Sqrt(float64(n))
-					kStart := int64(0.25 * (-float64(n)/float64(z) + 1))
-					kEnd := int64(0.25 * (float64(n)/float64(z) - 1))
+			state, err := prng.SampleState(nil, seed, uint16(tt.bitsPerSample), uint16(tt.numSamples))
+			if err != nil {
+				t.Fatalf("Sample(%x, 8 bits, 1000 samples) error %v", seed, err)
+			}
 
-					for k := kStart; k <= kEnd; k++ {
-						p -= normalCDF(float64((4*k+1)*z)/sqrtn) - normalCDF(float64((4*k-1)*z)/sqrtn)
-					}
+			// This test is primarily in place to confirm that the assembly
+			// implementation works as intended when converted to a
+			// high-level, more readable equivalent.
+			wantState := TestableCSPRNGState{
+				Seed:    new(big.Int).SetBytes(seed[:]),
+				Counter: big.NewInt(int64(tt.bitsPerSample*tt.numSamples/256 + 1)),
+				Remain:  big.NewInt(256 - int64(tt.bitsPerSample*tt.numSamples)%256),
+			}
 
-					kStart = int64(0.25 * (-float64(n)/float64(z) - 3))
-					for k := kStart; k <= kEnd; k++ {
-						p += normalCDF(float64((4*k+3)*z)/sqrtn) - normalCDF(float64((4*k+1)*z)/sqrtn)
-					}
+			// PRNG increments the counter and then fills the entropy pool
+			// with keccak256(seed||counter).
+			entropy := new(big.Int).Lsh(wantState.Seed, 256)
+			entropy.Add(entropy, wantState.Counter)
+			// It's important not to use entropy.Bytes() here as we MUST
+			// have exactly 64 bytes to be hashed.
+			buf := make([]byte, 64)
+			entropy.FillBytes(buf)
+			// Some of the entropy has depleted; we know how much from
+			// Remain.
+			wantState.Entropy = new(big.Int).Rsh(
+				new(big.Int).SetBytes(crypto.Keccak256(buf)),
+				uint(256-wantState.Remain.Uint64()),
+			)
 
-					if p < 0.01 {
-						t.Errorf("Cusum test failed with p=%.2f < 0.01", p)
-					}
-				}
-			})
-
-			t.Run("internal state", func(t *testing.T) {
-				// // This test is primarily in place to confirm that the assembly
-				// // implementation works as intended when converted to a
-				// // high-level, more readable equivalent.
-
-				seedInt := new(big.Int).SetBytes(seed[:])
-				two128 := new(big.Int).Lsh(big.NewInt(1), 128)
-
-				// first 128 bits of seed
-				carry := new(big.Int).Rsh(seedInt, 128)
-
-				// last 128 bits of seed
-				number := new(big.Int).Mod(seedInt, two128)
-
-				factor := new(big.Int).Sub(two128, big.NewInt(10408))
-
-				// Performing MWC updates
-				nRefills := (tt.numSamples * tt.bitsPerSample) / 128
-				for k := uint64(0); k < nRefills; k++ {
-					tmp := new(big.Int).Mul(factor, number)
-					tmp.Add(tmp, carry)
-
-					carry.Rsh(tmp, 128)
-					number.Mod(tmp, two128)
-				}
-
-				carry.Lsh(carry, 128)
-				entropy := new(big.Int).Add(carry, number)
-
-				wantState := TestablePRNGState{
-					Entropy: entropy,
-					Remain:  big.NewInt(128 - int64(tt.bitsPerSample*tt.numSamples)%128),
-				}
-
-				if diff := cmp.Diff(wantState, state, ethtest.Comparers()...); diff != "" {
-					t.Errorf("After %d samples of %d bits each; internal state diff (-want +got):\n%s", tt.numSamples, tt.bitsPerSample, diff)
-				}
-			})
+			if diff := cmp.Diff(wantState, state, ethtest.Comparers()...); diff != "" {
+				t.Errorf("After %d samples of %d bits each; internal state diff (-want +got):\n%s", tt.numSamples, tt.bitsPerSample, diff)
+			}
 		})
 	}
 }
 
 func TestBitLength(t *testing.T) {
-	_, prng := deploy(t)
 
-	for _, in := range []uint64{0, 1, 2, 3, 4, 5, 63, 64, 127, 128, 255, 256, math.MaxUint64} {
-		want := bits.Len64(in)
+	for _, ii := range getImplementations(t) {
+		t.Run(ii.name, func(t *testing.T) {
 
-		got, err := prng.BitLength(nil, new(big.Int).SetUint64(in))
-		if err != nil {
-			t.Errorf("BitLength(%d) error %v", in, err)
-			continue
-		}
+			for _, in := range []uint64{0, 1, 2, 3, 4, 5, 63, 64, 127, 128, 255, 256, math.MaxUint64} {
+				want := bits.Len64(in)
 
-		if got.Cmp(big.NewInt(int64(want))) != 0 {
-			t.Errorf("BitLength(%d) got %d; want %d", in, got, want)
-		}
+				got, err := ii.prng.BitLength(nil, new(big.Int).SetUint64(in))
+				if err != nil {
+					t.Errorf("BitLength(%d) error %v", in, err)
+					continue
+				}
 
+				if got.Cmp(big.NewInt(int64(want))) != 0 {
+					t.Errorf("BitLength(%d) got %d; want %d", in, got, want)
+				}
+
+			}
+		})
 	}
 }
 
 func TestReadLessThan(t *testing.T) {
-	_, prng := deploy(t)
 
-	const n = uint16(1e4)
+	for _, ii := range getImplementations(t) {
+		t.Run(ii.name, func(t *testing.T) {
 
-	for _, max := range []uint64{1, 2, 3, 7, 8, 127, 128, 1023, 1024} {
-		t.Run(fmt.Sprintf("%d samples < %d", n, max), func(t *testing.T) {
-			var seed [32]byte
+			const n = uint16(1e4)
 
-			bigMax := new(big.Int).SetUint64(max)
-			got, err := prng.ReadLessThan(nil, seed, bigMax, n)
-			if err != nil {
-				t.Fatalf("ReadLessThan() error %v", err)
-			}
+			for _, max := range []uint64{1, 2, 3, 7, 8, 127, 128, 1023, 1024} {
+				t.Run(fmt.Sprintf("%d samples < %d", n, max), func(t *testing.T) {
+					var seed [32]byte
 
-			for _, s := range got {
-				if s.Cmp(bigMax) != -1 {
-					t.Errorf("ReadLessThan(%d) returned sample %d out of range", max, s)
-				}
+					bigMax := new(big.Int).SetUint64(max)
+					got, err := ii.prng.ReadLessThan(nil, seed, bigMax, n)
+					if err != nil {
+						t.Fatalf("ReadLessThan() error %v", err)
+					}
+
+					for _, s := range got {
+						if s.Cmp(bigMax) != -1 {
+							t.Errorf("ReadLessThan(%d) returned sample %d out of range", max, s)
+						}
+					}
+				})
 			}
 		})
 	}
@@ -285,10 +457,13 @@ func TestStoreAndLoad(t *testing.T) {
 		var seed [32]byte
 		seed[31] = byte(i + 1)
 
-		sim, prng := deploy(t)
+		for _, ii := range getImplementations(t) {
+			t.Run(ii.name, func(t *testing.T) {
 
-		if _, err := prng.TestStoreAndLoad(sim.Acc(0), seed, tt.bits, tt.beforeStore); err != nil {
-			t.Errorf("StoreAndLoad(%#x, %d, %d) error %v", seed, tt.bits, tt.beforeStore, err)
+				if _, err := ii.prng.TestStoreAndLoad(ii.sim.Acc(0), seed, tt.bits, tt.beforeStore); err != nil {
+					t.Errorf("StoreAndLoad(%#x, %d, %d) error %v", seed, tt.bits, tt.beforeStore, err)
+				}
+			})
 		}
 	}
 }
