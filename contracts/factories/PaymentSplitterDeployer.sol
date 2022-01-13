ce0b31e5aa3867b61a6b48deb758140a0160e04b
Arran Schlosberg <me@arranschlosberg.com>
2022-01-13 13:58:24 +0000

    PaymentSplitterDeployer addresses for mainnet & Rinkeby + add README about factories and deployers.

--- contracts/factories/PaymentSplitterDeployer.sol
+++ contracts/factories/PaymentSplitterDeployer.sol
@@ -23,9 +23,11 @@ library PaymentSplitterDeployer {
             switch chainid()
             case 1 {
                 // mainnet
+                factory := 0xf034d6a4b1a64f0e6038632d87746ca24b79d325
             }
             case 4 {
                 // Rinkeby
+                factory := 0x633dc916D9f59cf4aA117dE2Bb8edF7752270EC0
             }
             case 1337 {
                 // The geth SimulatedBackend iff used with the ethier
---
+++ factories/README.md
@@ -0,0 +1,1 @@
+See `./contracts/factories/README.md`
