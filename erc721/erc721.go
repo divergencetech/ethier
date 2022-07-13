c6f2bc732b53f0a56bbfc26da460f68863370037
Arran Schlosberg <me@arranschlosberg.com>
2022-07-13 22:23:14 +0100

    Quick `rarity` binary to expose `erc721.Rarity()` output

--- erc721/erc721.go
+++ erc721/erc721.go
@@ -3,44 +3,21 @@ package erc721
 
 import (
 	"fmt"
-	"math/big"
-
-	"github.com/holiman/uint256"
 )
-
-// A TokenID is a uint256 Solidty tokenId.
-type TokenID [32]byte
-
-// TokenIDFromUint256 returns the 32-byte buffer underlying u, typed as a
-// TokenID pointer.
-func TokenIDFromUint256(u *uint256.Int) *TokenID {
-	t := TokenID(u.Bytes32())
-	return &t
-}
-
-// TokenIDFromBig is a convenience wrapper for
-// TokenIDFromUint256(uint256.FromBig(b)), returning an error if b overflows.
-func TokenIDFromBig(b *big.Int) (*TokenID, error) {
-	u, overflow := uint256.FromBig(b)
-	if overflow {
-		return nil, fmt.Errorf("%T %v overflows uint256", b, b)
-	}
-	return TokenIDFromUint256(u), nil
-}
-
-// TokenIDFromHex is a convenience wrapper for
-// TokenIDFromUint256(uint256.FromHex(b)), returning an error if FromHex() does.
-func TokenIDFromHex(h string) (*TokenID, error) {
-	u, err := uint256.FromHex(h)
-	if err != nil {
-		return nil, err
-	}
-	return TokenIDFromUint256(u), nil
-}
 
 // A Collection is a set of Metadata, each associated with a single uint256 (32
 // bytes).
 type Collection map[TokenID]*Metadata
+
+// CollectionFromMetadata assumes that each token's ID is its index in the
+// received Metadata slice, returning the respective Collection map.
+func CollectionFromMetadata(md []*Metadata) Collection {
+	c := make(Collection)
+	for i, m := range md {
+		c[*TokenIDFromInt(i)] = m
+	}
+	return c
+}
 
 // Metadata carries a parsed JSON payload from ERC721 metadata, compatible with
 // OpenSea.
