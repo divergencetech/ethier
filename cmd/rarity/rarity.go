718afcea081b1e02476d52ce5d8e734c8553aea3
Arran Schlosberg <me@arranschlosberg.com>
2022-07-13 22:23:14 +0100

    Quick `rarity` binary to expose `erc721.Rarity()` output

---
+++ cmd/rarity/rarity.go
@@ -0,0 +1,42 @@
+// The rarity binary calculates ERC721 metadata rarity, reading a JSON list from
+// stdin, and printing ranked rarity scores on stdout.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/divergencetech/ethier/erc721"
+)
+
+func main() {
+	buf, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("io.ReadAll(stdin): %v", err)
+	}
+
+	var md []*erc721.Metadata
+	if err := json.Unmarshal(buf, &md); err != nil {
+		log.Fatalf("json.Unmarshal(stdin, %T): %v", md, err)
+	}
+
+	type pair struct {
+		id    erc721.TokenID
+		score float64
+	}
+	var scores []pair
+	for id, score := range erc721.CollectionFromMetadata(md).Rarity(nil) {
+		scores = append(scores, pair{id, score})
+	}
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i].score > scores[j].score
+	})
+
+	for _, s := range scores {
+		fmt.Println(s.id.String(), s.score)
+	}
+}
---
+++ erc721/tokenid.go
@@ -0,0 +1,60 @@
+package erc721
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/holiman/uint256"
+)
+
+// A TokenID is a uint256 Solidty tokenId.
+type TokenID [32]byte
+
+// Uint256 returns id as a uint256.Int.
+func (id *TokenID) Uint256() *uint256.Int {
+	// This is guaranteed to never overflow, so it's safe to ignore the flag.
+	i, _ := uint256.FromBig(new(big.Int).SetBytes(id[:]))
+	return i
+}
+
+// String returns a decimal text representation of id.
+func (id *TokenID) String() string {
+	return new(big.Int).SetBytes(id[:]).Text(10)
+}
+
+// TokenIDFromUint256 returns the 32-byte buffer underlying u, typed as a
+// TokenID pointer.
+func TokenIDFromUint256(u *uint256.Int) *TokenID {
+	t := TokenID(u.Bytes32())
+	return &t
+}
+
+// TokenIDFromBig returns TokenIDFromUint256(uint256.FromBig(b)), or an error if
+// b overflows 256 bits.
+func TokenIDFromBig(b *big.Int) (*TokenID, error) {
+	u, overflow := uint256.FromBig(b)
+	if overflow {
+		return nil, fmt.Errorf("%T %v overflows uint256", b, b)
+	}
+	return TokenIDFromUint256(u), nil
+}
+
+// TokenIDFromHex returns TokenIDFromUint256(uint256.FromHex(b)), or any error
+// returned by FromHex().
+func TokenIDFromHex(h string) (*TokenID, error) {
+	u, err := uint256.FromHex(h)
+	if err != nil {
+		return nil, err
+	}
+	return TokenIDFromUint256(u), nil
+}
+
+// TokenIDFromUint64 returns TokenIDFromUint256(uint256.NewInt(u)).
+func TokenIDFromUint64(u uint64) *TokenID {
+	return TokenIDFromUint256(uint256.NewInt(u))
+}
+
+// TokenIDFromInt returns TokenIDFromUint256(uint256.NewInt(uint64(i))).
+func TokenIDFromInt(i int) *TokenID {
+	return TokenIDFromUint256(uint256.NewInt(uint64(i)))
+}
