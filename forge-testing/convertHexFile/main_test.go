6232dd71134379c200dcb6ed0d5560a8b49517c4
Arran Schlosberg <me@arranschlosberg.com>
2022-11-21 16:50:43 +0000

    Suggestions for PR 66

--- forge-testing/convertHexFile/main_test.go
+++ forge-testing/convertHexFile/main_test.go
@@ -1,54 +1,47 @@
 package main
 
 import (
-	"io"
+	"bytes"
 	"os"
-	"reflect"
 	"testing"
 )
 
 func TestConvertFile(t *testing.T) {
 	tests := []struct {
-		FileContent string
-		Want        []byte
+		fileContent string
+		bufferSize  int
+		want        []byte
 	}{
-		{"0x0102abcd", []byte{0x01, 0x02, 0xab, 0xcd}},
-		{"abcdef", []byte{0xab, 0xcd, 0xef}},
+		{"0102abcd", 4, []byte{0x01, 0x02, 0xab, 0xcd}},
+		{"0x0102abcd", 32, []byte{0x01, 0x02, 0xab, 0xcd}},
+		{"abcdef", 32, []byte{0xab, 0xcd, 0xef}},
+		{"0xabcdef", 2, []byte{0xab, 0xcd, 0xef}},
 	}
 	for _, tt := range tests {
-		// Write test file
+		t.Run(tt.fileContent, func(t *testing.T) {
+			// Write test file
+			file, err := os.CreateTemp("", "")
+			if err != nil {
+				t.Fatalf(`os.CreateTemp("", "") error %v`, err)
+			}
+			defer os.Remove(file.Name())
+			defer file.Close()
 
-		file, err := os.CreateTemp("", "")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer os.Remove(file.Name())
-		defer file.Close()
+			if _, err = file.WriteString(tt.fileContent); err != nil {
+				t.Fatalf("%T.WriteString(%q) error %v", file, tt.fileContent, err)
+			}
+			if err = convertHexFile(file.Name(), tt.bufferSize); err != nil {
+				t.Fatalf("convertHexFile([tmp file], %d) error %v", tt.bufferSize, err)
+			}
 
-		_, err = file.WriteString(tt.FileContent)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		err = convertHexFile(file.Name())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// Test new content
-
-		file, err = os.Open(file.Name())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		got, err := io.ReadAll(file)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if !reflect.DeepEqual(got, tt.Want) {
-			t.Errorf("Incorrect file content after conversion, want %x, got %x", tt.Want, got)
-		}
+			// Test new content
+			got, err := os.ReadFile(file.Name())
+			if err != nil {
+				t.Fatalf("os.ReadFile([tmp file]) error %v", err)
+			}
+			if !bytes.Equal(got, tt.want) {
+				t.Errorf("convertHexFile() wrote %+x; want %+x", got, tt.want)
+			}
+		})
 	}
 }
