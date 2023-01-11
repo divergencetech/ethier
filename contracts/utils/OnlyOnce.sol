ed7fbb28ab2c243dbbd5a1e43a7b3f9da4009823
cxkoda <dave@proof.xyz>
2023-01-11 13:12:46 +0100

    Refactor + add comments

--- contracts/utils/OnlyOnce.sol
+++ contracts/utils/OnlyOnce.sol
@@ -2,26 +2,57 @@
 // Copyright (c) 2023 the ethier authors (github.com/divergencetech/ethier)
 pragma solidity >=0.8.0 <0.9.0;
 
+import "forge-std/console2.sol";
+
 contract OnlyOnce {
+    bytes32 private constant _LEFTMOST_FOUR_BYTES_MASK =
+        0xFFFFFFFF00000000000000000000000000000000000000000000000000000000;
+
     /**
      * @notice Thrown if a function can only be executed once.
      */
-    error FunctionAlreadyExecuted(bytes4 selector);
+    error FunctionAlreadyExecuted(bytes32 selector);
 
     /**
      * @notice Keeps track of function executions.
      */
-    mapping(bytes4 => bool) private _functionAlreadyExecuted;
+    mapping(bytes32 => bool) private _functionAlreadyExecuted;
+
+    /**
+     * @notice Ensures that a given identifier has not been marked as already
+     * executed and marks it as such.
+     */
+    function _ensureOnlyOnce(bytes32 identifier) private {
+        if (_functionAlreadyExecuted[identifier]) {
+            revert FunctionAlreadyExecuted(identifier);
+        }
+        _functionAlreadyExecuted[identifier] = true;
+    }
 
     /**
      * @notice Ensures that the modified function can only be executed once.
-     * @param selector The selector of the wrapped function.
+     * @param identifier A generic UNIQUE identifier to mark the wrapped
+     * function as used. Typically the EVM function selector of the wrapped
+     * function (padded to the right with zeroes).
      */
-    modifier onlyOnce(bytes4 selector) {
-        if (_functionAlreadyExecuted[selector]) {
-            revert FunctionAlreadyExecuted(selector);
+    modifier onlyOnceByIdentifier(bytes32 identifier) {
+        _ensureOnlyOnce(identifier);
+        _;
+    }
+
+    /**
+     * @notice Ensures that the modified function can only be executed once.
+     * @dev This modifier MUST NOT be used on functions that are either public
+     * or internal. The modifier uses the function selector of the current
+     * calldata context as identifier, which can have unintended side-effects
+     * for internally used functions.
+     */
+    modifier onlyOnce() {
+        bytes32 selector;
+        assembly {
+            selector := and(calldataload(0), _LEFTMOST_FOUR_BYTES_MASK)
         }
-        _functionAlreadyExecuted[selector] = true;
+        _ensureOnlyOnce(selector);
         _;
     }
 }
--- tests/utils/OnlyOnce.t.sol
+++ tests/utils/OnlyOnce.t.sol
@@ -7,17 +7,19 @@ import "forge-std/console2.sol";
 
 import {OnlyOnce} from "../../contracts/utils/OnlyOnce.sol";
 
+// solhint-disable no-empty-blocks
 contract OnlyOnceConsumer is OnlyOnce {
-    function limitedFunction1()
-        external
-        onlyOnce(OnlyOnceConsumer.limitedFunction1.selector)
-    {} // solhint-disable-line no-empty-blocks
+    function limitedFunction1() external onlyOnce {}
 
-    function limitedFunction2()
+    function limitedFunction2(uint256) external onlyOnce {}
+
+    function limitedFunction3()
         external
-        onlyOnce(OnlyOnceConsumer.limitedFunction2.selector)
-    {} // solhint-disable-line no-empty-blocks
+        onlyOnceByIdentifier(OnlyOnceConsumer.limitedFunction3.selector)
+    {}
 }
+
+// solhint-enable no-empty-blocks
 
 contract OnlyOnceTest is Test {
     OnlyOnceConsumer public c;
@@ -26,24 +28,40 @@ contract OnlyOnceTest is Test {
         c = new OnlyOnceConsumer();
     }
 
-    function testCannotCallFunctionsTwice() public {
+    function _testCannotCallTwice(function() external func) internal {
+        func();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OnlyOnce.FunctionAlreadyExecuted.selector,
+                func.selector
+            )
+        );
+        func();
+    }
+
+    function _testCannotCallTwice(
+        function(uint256) external func,
+        uint256 param1,
+        uint256 param2
+    ) internal {
+        func(param1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OnlyOnce.FunctionAlreadyExecuted.selector,
+                func.selector
+            )
+        );
+        func(param2);
+    }
+
+    function testCannotCallFunctionsTwice(uint256 param1, uint256 param2)
+        public
+    {
         // This test also ensures that calling one limited function does not
         // affect the executability of the other.
 
-        function() external[2] memory funcs = [
-            c.limitedFunction1,
-            c.limitedFunction2
-        ];
-
-        for (uint256 i; i < funcs.length; ++i) {
-            funcs[i]();
-            vm.expectRevert(
-                abi.encodeWithSelector(
-                    OnlyOnce.FunctionAlreadyExecuted.selector,
-                    OnlyOnceConsumer.limitedFunction1.selector
-                )
-            );
-            funcs[i]();
-        }
+        _testCannotCallTwice(c.limitedFunction1);
+        _testCannotCallTwice(c.limitedFunction2, param1, param2);
+        _testCannotCallTwice(c.limitedFunction3);
     }
 }
